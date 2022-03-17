5a64d89fd455fb95635c9c5cadb6d028a4b99e38
Toheeb <aladetoheeb5@gmail.com>
2022-03-17 17:38:16 +0100

    Doesnt print 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - A function that prints the
- * numbers, from 0 to 9, followed by a new line.
+ * print_most_numbers - A function that prints
+ * the numbers, from 0 to 9, except  2 and 4
  */
 
 void print_most_numbers(void)
 {
-int i;
+	int i;
 
-for (i = '0'; i <= '9'; i++)
-{
-	if (i != '2' && i != '4')
-		_putchar(i);
-}
-_putchar('\n');
+	for (i = '0'; i < '9'; i++)
+	{
+		if(i != 2 && i != 4)
+			_putchar(i);
+	}
+	_putchar('\n');
 }
