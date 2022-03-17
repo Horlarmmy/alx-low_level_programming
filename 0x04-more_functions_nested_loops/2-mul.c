d7fa2e20e121c37dda3b5c8408d4457b944ffe25
Toheeb <aladetoheeb5@gmail.com>
2022-03-17 17:01:08 +0100

    Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - A function that multiplies two integer.
+ * @a, b: The numbers to be multiplied
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+int mul(int a, intb)
+{
+	int j = a * b;
+	return j;
+}
