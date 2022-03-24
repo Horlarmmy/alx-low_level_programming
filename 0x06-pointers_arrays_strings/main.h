78cb67eaf3466efa672a6e39b924b1d18ae94312
Toheeb <aladetoheeb5@gmail.com>
2022-03-24 17:45:13 +0100

    Hot fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,3 +6,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 void print_number(int n);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
