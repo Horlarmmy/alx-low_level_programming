1ecc9512c9c15d17a7aaef28853d9dfe386fd5ea
Toheeb <aladetoheeb5@gmail.com>
2022-03-31 22:54:42 +0100

    Hot fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,7 +4,8 @@
  *  * _puts_recursion - a function that prints a
  *   * string, followed by a new line
  *    * @s: string to be printed
- *     */
+ *     
+ */
 
 void _puts_recursion(char *s)
 {
