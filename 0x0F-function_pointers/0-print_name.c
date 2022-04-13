3c3c0f7618da34f2ba79c37f11433e82762d3b5d
Toheeb <aladetoheeb5@gmail.com>
2022-04-13 18:57:09 +0100

    Initial commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/*
+ * print_name: print a name
+ * @name: the name to print
+ * @f: finction that print the name
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
